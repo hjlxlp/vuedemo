7c44162ec6ccf1eb892cf1632b0e5e61d97160ad
huangjiale <huangjiale@aikesaisi.com>
2023-04-28 15:47:59 +0800

    测试

---
+++ src/view/index.vue
@@ -0,0 +1,193 @@
+<template>
+  <el-table :data="tableData" border stripe style="width: 100% ">
+
+    <el-table-column prop="classname" label="价值观" width="180">
+    </el-table-column>
+    <el-table-column prop="kpiname" label="Our Behaviors" width="180">
+    </el-table-column>
+    <el-table-column prop="leading" label="领导自己" width="180">
+    </el-table-column>
+
+    <el-table-column :label="item" v-for="(item, index) in header" :key="index"  align="center">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.kpilist[index].content" @change="rowItemChange(scope.row.kpilist[index])"></el-input>
+      </template>
+    </el-table-column>
+
+    <el-table-column prop="resultname" label="结果" width="180">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.resultname" @change="rowChange(scope.row)"></el-input>
+      </template>
+    </el-table-column>
+    <el-table-column prop="result" label="是/否" width="180">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.result" @change="rowChange(scope.row)"></el-input>
+      </template>
+    </el-table-column>
+    <el-table-column prop="remark" label="备注" width="180">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.remark" @change="rowChange(scope.row)"></el-input>
+      </template>
+    </el-table-column>
+
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: "indexName",
+  data() {
+    return {
+      header : ["CG3","CG4","CG5"],
+      tableData: [
+        {
+          "kpilist": [
+            {
+              "kpiid": 6,
+              "autoid": 17,
+              "content": "8",
+              "hrlevel": "CG3"
+            },
+            {
+              "kpiid": 6,
+              "autoid": 18,
+              "content": "10",
+              "hrlevel": "CG4"
+            },
+            {
+              "kpiid": 6,
+              "autoid": 19,
+              "content": "12",
+              "hrlevel": "CG5"
+            }
+          ],
+          "classname": "彼此信任",
+          "orderindex": 3,
+          "instanceid": "ffceae93-4a54-42b3-a0b1-58027f022c2b",
+          "kpiid": 6,
+          "kpiname": "质量",
+          "resultname": "1",
+          "result": "2",
+          "remark": "3",
+          "leading": "质量导向"
+        },
+        {
+          "kpilist": [
+            {
+              "kpiid": 7,
+              "autoid": 20,
+              "content": "8",
+              "hrlevel": "CG3"
+            },
+            {
+              "kpiid": 7,
+              "autoid": 21,
+              "content": "10",
+              "hrlevel": "CG4"
+            },
+            {
+              "kpiid": 7,
+              "autoid": 22,
+              "content": "12",
+              "hrlevel": "CG5"
+            }
+          ],
+          "classname": "全力拼搏",
+          "orderindex": 3,
+          "instanceid": "ffceae93-4a54-42b3-a0b1-58027f022c2b",
+          "kpiid": 7,
+          "kpiname": "结果导向",
+          "resultname": "",
+          "result": "",
+          "remark": "",
+          "leading": "规划与组织"
+        },
+        {
+          "kpilist": [
+            {
+              "kpiid": 8,
+              "autoid": 23,
+              "content": "8",
+              "hrlevel": "CG3"
+            },
+            {
+              "kpiid": 8,
+              "autoid": 24,
+              "content": "10",
+              "hrlevel": "CG4"
+            },
+            {
+              "kpiid": 8,
+              "autoid": 25,
+              "content": "12",
+              "hrlevel": "CG5"
+            }
+          ],
+          "classname": "自主而为",
+          "orderindex": 3,
+          "instanceid": "ffceae93-4a54-42b3-a0b1-58027f022c2b",
+          "kpiid": 8,
+          "kpiname": "主人翁意识",
+          "resultname": "",
+          "result": "",
+          "remark": "",
+          "leading": "展现主人翁意识"
+        },
+        {
+          "kpilist": [
+            {
+              "kpiid": 9,
+              "autoid": 26,
+              "content": "8",
+              "hrlevel": "CG3"
+            },
+            {
+              "kpiid": 9,
+              "autoid": 27,
+              "content": "10",
+              "hrlevel": "CG4"
+            },
+            {
+              "kpiid": 9,
+              "autoid": 28,
+              "content": "12",
+              "hrlevel": "CG5"
+            }
+          ],
+          "classname": "同舟共济",
+          "orderindex": 3,
+          "instanceid": "ffceae93-4a54-42b3-a0b1-58027f022c2b",
+          "kpiid": 9,
+          "kpiname": "协作",
+          "resultname": "",
+          "result": "",
+          "remark": "",
+          "leading": "协作"
+        }
+      ]
+    }
+  },
+  methods: {
+    rowChange(row) {
+      // todo 调用后端接口，根据kpiid，修改resultname，result，remark
+      // row代表一行
+      //alert(row);
+      console.log("kpiid：" + row.kpiid)
+      console.log("resultname：" + row.resultname)
+      console.log("result：" + row.result)
+      console.log("remark：" + row.remark)
+    },
+    rowItemChange(rowItem) {
+      // todo 调用后端接口，根据autoid，修改content
+      // rowItem代表一行里面的某个目标
+      //alert(rowItem);
+      console.log("autoid：" + rowItem.autoid)
+      console.log("content：" + rowItem.content)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-table :data="tableData">
+      <el-table-column
+          v-for="column in columns"
+          :key="column.prop"
+          :prop="column.prop"
+          :label="column.label"
+      >
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "testName",
+  data() {
+    return {
+      columns: [
+        { label: '姓名', prop: 'name' },
+        { label: '年龄', prop: 'age' },
+      ],
+      tableData: [
+        { name: '张三', age: 18 },
+        { name: '李四', age: 20 },
+      ],
+    };
+  },
+  methods: {
+    // 动态添加列
+    addColumn() {
+      this.columns.push({ label: '性别', prop: 'gender' });
+      this.tableData.forEach((item) => {
+        this.$set(item, 'gender', '男');
+      });
+    },
+  },
+};
+</script>
